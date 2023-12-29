6182ef16cb48efe0e19440fd3c8eb7ed1003f3e3
krizhnaa <krizhnaprem@gmail.com>
2023-12-29 16:39:33 +0530

    If the user enters a number, it catches the error and prompts the user to enter words containing letter

--- main.py
+++ main.py
@@ -7,9 +7,15 @@ nato_df = pd.read_csv("nato_phonetic_alphabet.csv")
 nato_dict = { row.letter:row.code for (index, row) in nato_df.iterrows()}
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+success = False
 
-word = input("Enter a word : ").upper()
+while not success:
+    word = input("Enter a word : ").upper()
+    try:
+        List = [ nato_dict[letter] for letter in word]
+    except KeyError:
+        print("Enter a word consisting of letters")
+    else:
+        print(List)
+        success = True
 
-List = [ nato_dict[letter] for letter in word]
-
-print(List)
